9b577ac1b2cad1a88aff08bad3c0a170b9997d23
adrianuceta <aucegam0210@g.educaand.es>
2025-02-04 10:02:00 +0100

    Más cambios en manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,15 @@
-# servicio_manteminiento/__manifest__.py
 {
-
     'name': 'Servicio de Mantenimiento',
     'version': '1.0',
     'author': 'Adrián Uceta Gamaza',
     'category': 'Custom',
     'summary': 'Gestión de Mantenimiento de Productos',
-    'depends': ['base', 'garantia_producto'],
+    'depends': ['base', 'garantias'],  # Cambiado aquí
     'data': [
-    'security/ir.model.access.csv',
-    'views/servicio_mantenimiento_views.xml',
+        'security/ir.model.access.csv',
+        'views/servicio_mantenimiento_views.xml',
     ],
     'icon': '/garantias/static/description/icon55.png',
     'installable': True,
     'application': True,
-
 }
